0e54e46643c4d254650d0231d1482d0715b75d47
Anshuman <Anshuman>
2020-02-12 14:44:04 +0530

    Adding button in jsx and checking issue due to uper case tag name use

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent'
 import SecondComponent from './components/learning-examples/SecondComponent'
 import ThirdComponent from './components/learning-examples/ThirdComponent'
+import Counter from './components/counter/Counter'
 import './App.css';
 
 class App extends Component{
   render(){
     return <div className="App">
         My Todo Application
-        <FirstComponent />
-        <SecondComponent />
-        <ThirdComponent />
+        <Counter/>
       </div>
     ;
   }
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Function component
+function Counter (){
+    return (
+      <div className="counter">
+          <button>+1</button>
+      </div>
+    );
+}
+
+export default Counter
